55957ecd592dbc81e9bcfb5e13892979a0bf4999
Piccolo <kabiryusufbashir@gmail.com>
2021-05-22 19:31:44 +0100

    Creating the NavBar

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -3,12 +3,18 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @yield('page-meta')
     <title>@yield('title')</title>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
     <link href="{{ asset('images/favicon.ico') }}" rel="icon">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
-    Hello
+    <!-- Navigation Bar -->
+    <div>
+        <div>Logo</div>
+        <div>Navigation</div>
+        <div>Button</div>
+    </div>
 </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,11 @@
 @extends('layouts.template')
 
+@section('page-meta')
+    <meta name="description" content="A Non-Governmental Organization with the aim to build a northeast Nigeria in which every child; victims of terror attacks attains the right to educational survival, protection development and participation.">
+    <meta name="keywords" content="NGO, Borno, Maiduguri, North-east, Nigeria">
+@endsection
+
 @section('title')
     SWAT Foundation - Home
 @endsection
+
