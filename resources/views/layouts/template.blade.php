dfc3cae2d71983e8cde1ef6a86fb478d545747c4
Piccolo <kabiryusufbashir@gmail.com>
2021-05-23 15:45:12 +0100

    Create the slogan and started working on the homepage contents

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -5,9 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     @yield('page-meta')
     <title>@yield('title')</title>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
     <link href="{{ asset('images/favicon.ico') }}" rel="icon">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/main.css') }}" rel="stylesheet">
 </head>
 <body>
     <!-- Navigation Bar -->
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,7 +10,24 @@
 @endsection
 
 @section('body-content')
-    <div>
-        <img class="object-cover w-full" src="{{ asset('images/bg-1.jpg') }}" alt="SWAT Students">
+    <div class="relative">
+        <img class="object-cover w-full h-screen md:h-auto" src="{{ asset('images/bg-1.jpg') }}" alt="SWAT Students">
+        <div class="w-full px-4 leading-snug absolute top-32 md:top-40 text-white text-5xl items-center flex justify-center">
+            <div id="slogan" class="text-center">
+                <span class="text-7xl">Save</span> <br>
+                <span class="text-blue-400">With</span> A Touch <span class="text-blue-400">Foundation</span>
+            </div>
+        </div>
+    </div>
+    <div class="w-full text-center py-4 px-8 md:px-24 shadow">
+        <div>
+            <img class="mx-auto w-72 md:w-96" src="{{ asset('images/logo.png') }}" alt="Foundation Logo">
+        </div>
+        <div>
+            <p class="text-2xl">
+                "Community service gives me a valuable opportunity to walk into a different community that is less familiar to me but just as colorful and most importantly, in need"
+            </p>
+            <i>Caroline Landry</i>
+        </div>
     </div>
 @endsection
